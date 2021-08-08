c89169ff8c1d8860e553db1ff2e6178c9e657e73
jiajie chen <ga-kit@hotmail.com>
2021-08-08 14:12:16 -0700

    created post model

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
+  username: 'root',
+  password: 'kaka6652',
   port: 3306
 });
 
