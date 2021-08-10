2485d75c634dddfb501e0b03c478eb253c1aa2d5
jiajie chen <ga-kit@hotmail.com>
2021-08-09 23:37:11 -0700

    comment feature added

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
 
+
 // create associations
 User.hasMany(Post, {
   foreignKey: 'user_id'
@@ -40,4 +41,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Vote, Comment };
