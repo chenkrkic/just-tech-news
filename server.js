1c7c765026cb3e8dfd46605d2ab354afe35620c3
jiajie chen <ga-kit@hotmail.com>
2021-08-15 18:35:40 -0700

    edit post page

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
+
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
+    
   })
 };
 
